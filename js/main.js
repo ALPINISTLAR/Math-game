f78f9c1886185a676ec5b42a1b35b6d4599b6b1f
ALPINISTLAR <niveam424@gmail.com>
2023-12-09 17:37:34 +0200

    add game interface

--- js/main.js
+++ js/main.js
@@ -1,19 +1,35 @@
+const elCondition = document.querySelector('.condition');
+const elButton = document.querySelector('.toggle-button');
+
 let buttonText = '+';
-const button = document.querySelector('.toggle-button');
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 9) + 1;
+}
+
+
 
 function toggleText() {
+  const x = getRandomNumber();
+  const y = getRandomNumber();
+
+
   switch (buttonText) {
     case '+':
       buttonText = '-';
+      elCondition.textContent = `${x} va ${y} ning ayirmasini hisoblang!`;
       break;
     case '-':
       buttonText = 'x';
+      elCondition.textContent = `${x} va ${y} ning ko'paytmasini hisoblang!`;
       break;
     case 'x':
       buttonText = '+';
+      elCondition.textContent = `${x} va ${y} ning yig'indisini hisoblang!`;
       break;
     default:
       buttonText = '+';
+      elCondition.textContent = `${x} va ${y} ning yig'indisini hisoblang!`;
   }
-  button.textContent = buttonText;
+  elButton.textContent = buttonText;
 }
